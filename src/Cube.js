6017b22f2844cfceb52c0045ff6499bed013d2c1
Nick Corfmat <ncorfmat@ucsc.edu>
2025-01-29 16:26:19 -0800

    Step 2.5 done

--- src/Cube.js
+++ src/Cube.js
@@ -14,13 +14,30 @@ class Cube {
     // pass the matrix to u_ModelMatrix attribute
     gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
 
+    // Top face
     drawTriangle3D([0, 0, 0, 1, 1, 0, 1, 0, 0]);
     drawTriangle3D([0, 0, 0, 0, 1, 0, 1, 1, 0]);
 
     gl.uniform4f(u_FragColor, rgba[0]*0.9, rgba[1]*0.9, rgba[2]*0.9, rgba[3]);
 
-    // Draw
-    drawTriangle3D([0, 1, 0, 0, 1, 1, 1, 1, 1]);
-    drawTriangle3D([0, 1, 0, 1, 1, 1, 1, 1, 0]);
+    // Front face
+    drawTriangle3D([0, 0, 0, 1, 0, 0, 1, 0, -1]);
+    drawTriangle3D([0, 0, 0, 1, 0, -1, 0, 0, -1]);
+
+    // Back face
+    drawTriangle3D([0, 1, 0, 1, 1, 0, 1, 1, -1]);
+    drawTriangle3D([0, 1, 0, 1, 1, -1, 0, 1, -1]);
+
+    // Left face
+    drawTriangle3D([0, 0, 0, 0, 0, -1, 0, 1, -1]);
+    drawTriangle3D([0, 0, 0, 0, 1, -1, 0, 1, 0]);
+
+    // Right face
+    drawTriangle3D([1, 0, 0, 1, 0, -1, 1, 1, -1]);
+    drawTriangle3D([1, 0, 0, 1, 1, -1, 1, 1, 0]);
+
+    // Bottom face
+    drawTriangle3D([0, 0, -1, 1, 0, -1, 1, 1, -1]);
+    drawTriangle3D([0, 0, -1, 1, 1, -1, 0, 1, -1]);
   }
 }
